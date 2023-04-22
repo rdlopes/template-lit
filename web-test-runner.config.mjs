850ba97cb687dd699d678affab7f4a4ed86c9400
Rui LOPES <rui.d.lopes@me.com>
2023-04-22 13:52:41 +0200

    fix(tests): remove firefox

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -2,14 +2,13 @@ import { playwrightLauncher } from '@web/test-runner-playwright';
 import { esbuildPlugin }      from "@web/dev-server-esbuild";
 
 export default {
-  plugins: [esbuildPlugin({ ts: true })],
+  plugins: [esbuildPlugin({ ts: true, target: 'auto' })],
   files: 'src/**/*.test.ts',
   nodeResolve: true,
   coverage: true,
   concurrentBrowsers: 3,
   browsers: [
     playwrightLauncher({ product: 'chromium' }),
-    playwrightLauncher({ product: 'firefox' }),
     playwrightLauncher({ product: 'webkit' }),
   ],
 };
